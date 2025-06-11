df3d45d9414fb58af76884eaf759def388248bfb
kaktycEvdo <ilya.tretyakov.2005@bk.ru>
2025-06-11 11:55:14 +0700

    metrika thing? 2

--- simple_erp/app/root.jsx
+++ simple_erp/app/root.jsx
@@ -8,6 +8,7 @@ import {
 } from "react-router";
 import { Header } from "./components/UI/Header.jsx";
 import { Footer } from "./components/UI/Footer.jsx";
+import { YMInitializer } from 'react-yandex-metrika';
 
 import stylesheet from "./app.css?url";
 
@@ -21,19 +22,7 @@ export function Layout({ children }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <script type="text/javascript" >
-          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-          m[i].l=1*new Date();
-          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-          ym(102575796, "init", {
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true
-          });
-        </script>
+        <YMInitializer accounts={[102575796]} />
         <noscript><div><img src="https://mc.yandex.ru/watch/102575796" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
         <Meta />
         <Links />
